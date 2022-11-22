74f00f1e81a0287428d6c6bc8dad3db10479029c
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-11-22 21:07:08 +0530

    h

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -36,8 +36,9 @@ return (
     <div>
       <div className='container w-3/4 h-3/4 mx-auto text-center'>
         <h1 className='text-xl font-mono my-6'>QR Scanner</h1>
+        <div className='h-2/4'>
       <video id="qr" className=' rounded-md md:w-2/4 md:h-full  w-full  mx-auto'  ></video>
-    
+    </div>
     {button}
     <div>
      <h1>{resultTxt}</h1>
