8177285495b685890fb9f3449642bd53a1ae6a10
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-11-22 21:48:26 +0530

    j

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -3,8 +3,8 @@ import QrScanner from 'qr-scanner';
 function Reader() {
   /* When qr code get an result */
   let done=(result,qrScanner)=>{
-    setResultTxt(result)
-    setButton(resultTxt)
+    setResultTxt([result,...resultTxt])
+  
     
     console.log(result)
    
@@ -28,7 +28,7 @@ setButton("")
   const [started,setStarted]=useState(false)
   /* to change scannow button to scanning when scan started */
   const [button,setButton]=useState( <button className='p-2 m-2 rounded primary-bg secondary-text' onClick={scan}>Scan Now</button>)
-  const [resultTxt,setResultTxt]=useState("")
+  const [resultTxt,setResultTxt]=useState([])
   
 useEffect(()=>{
   console.log(resultTxt)
@@ -40,10 +40,15 @@ return (
         
       <video id="qr" className=' rounded-md md:w-2/4 md:h-full    mx-auto'  ></video>
     
-    {button}
+
     <div>
-     <h1>{resultTxt}</h1>
+     {resultTxt!=""?resultTxt.map((r)=>{
+return (
+<h1>r</h1>
+)
+     }):null}
     </div>
+    {button}
 </div>
     </div>
   )
