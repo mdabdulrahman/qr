47722cb036a2d57f263e713a7e7ca67609a970e7
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-11-25 19:50:43 +0530

    up

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -41,6 +41,9 @@ let copytxt=(e)=>{
 navigator.clipboard.writeText(resultTxt[0])
 alert("Text copied")
 }
+let ico=<svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"  >
+<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+</svg>
 return (
     <div className='font-["Poppins"]'>
       <div className='container   w-full h-full mx-auto text-center'>
@@ -58,12 +61,10 @@ return (
     <div className='w-full absolute font-semibold text-sm overflow-x-scroll bottom-0 bg-gray-200'>
      {resultTxt.length!=1?resultTxt.map((r)=>{
 return (
-<h1  className='my-4 flex gap-2 items-center justify-center'>
-<svg onClick={(e)=>{copytxt(e)}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"  >
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
-</svg>
+<h1 onClick={(e)=>{copytxt(e)}} className='my-4 flex gap-2 items-center justify-center'>
 
-  {r}</h1>
+
+  {ico}{r}</h1>
 )
      }):resultTxt[0]}
     </div>
