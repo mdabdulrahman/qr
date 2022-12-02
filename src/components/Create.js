a6dd5dc89c07a0921bcc5db2038871b33247c4f9
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-12-02 21:13:20 +0530

    big change

--- src/components/Create.js
+++ src/components/Create.js
@@ -21,7 +21,7 @@ function Create() {
 console.log(img.current.toDataURL("image/jpeg"))
 setdownloadbtn(
   <div className='mx-auto w-1/2'>
-  <a href={img.current.toDataURL("image/jpeg")} className="bg-red-400  text-white  px-6 py-3" download="qrcode">Download</a>
+  <a href={img.current.toDataURL("image/jpg")} className="bg-red-400  text-white  px-6 py-3" download="qrcode">Download</a>
 </div>
 )
   }
